5963e86804b8200afbe09f1b58b07a6fed3a67b9
Milind Soni <46266943+milind-soni@users.noreply.github.com>
2025-03-17 16:36:30 +0530

    Add files via upload

---
+++ clientai/src/app/page.tsx
@@ -0,0 +1,12 @@
+import MCPClient from "./components/MCPClient/MCPClient";
+import styles from "../style/page.module.css";
+
+import { useParams } from "next/navigation";
+
+export default async function Page() {
+  return (
+    <div className={styles.page}>
+      <MCPClient />
+    </div>
+  );
+}
---
+++ clientai/src/hooks/useChat.ts
@@ -0,0 +1,5 @@
+"use client";
+
+import { useState, useCallback, useRef } from "react";
+
+export function useChat({ apiRoute = "/api/chat", initialTokens = [] }) {}
